0c0b5305b415559d080e705fc9aeb346a58ca3d7
KMenshchikova <kmenshchikova@timepath.ru>
2022-03-23 09:40:23 +0500

    Конфиг

--- ember-app/config/environment.js
+++ ember-app/config/environment.js
@@ -2,11 +2,11 @@
 
 module.exports = function(environment) {
   // Replace this local address to remote when backed will be published.
-  var backendUrl = 'http://localhost:6500';
+  var backendUrl = 'https://teststand.t-mobis.ru';
 
   if (environment === 'development-loc') {
     // Use `ember s -e development-loc` command for local backend usage.
-    backendUrl = 'http://localhost:6500';
+    backendUrl = 'https://teststand.t-mobis.ru';
   }
 
 
